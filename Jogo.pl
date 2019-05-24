04b7629efa2e132a944d68752a78362a31a238bb
Gabriel Augusto <g.requenareis@gmail.com>
2019-05-24 04:49:40 -0300

    Verifica se jogo chegou ao fim

--- Jogo.pl
+++ Jogo.pl
@@ -24,19 +24,28 @@ achaY(_,_,[],_) :- false.
 achaY(Y,Aux,[H|_],H) :- Y == Aux.
 achaY(Y,Aux,[_|HT],R) :- Aux1 is Aux + 1, achaY(Y,Aux1,HT,R).
 
-%%Descobre opções de caminhos
+%%Descobre vizinhos de (X,Y)
 elementosVizinhos(X,Y,[S,I,D,E]) :- elementoSuperior(X,Y,S), elementoInferior(X,Y,I),elementoDireita(X,Y,D),elementoEsquerda(X,Y,E).
 elementoSuperior(X,Y,Elemento) :- N is X-1,recuperaElemento(N,Y,Elemento).
 elementoInferior(X,Y,Elemento)  :- N is X+1,recuperaElemento(N,Y,Elemento).
 elementoDireita(X,Y,Elemento) :- N is Y+1,recuperaElemento(X,N,Elemento).
 elementoEsquerda(X,Y,Elemento) :- N is Y-1,recuperaElemento(X,N,Elemento).
 
+%%Verifica se jogo chegou ao fim
+fimDeJogo() :- matrizBase(Matriz), verificaFim(Matriz,X), X < 2.
+verificaFim([],X) :- X is 0, !.
+verificaFim([H|T],X) :- verificaFimColuna(H,X1), verificaFim(T,X2), X is X1 + X2.
+verificaFimColuna([],X) :- X is 0, !.
+verificaFimColuna([H|T],R) :- H == -1, verificaFimColuna(T, R).
+verificaFimColuna([H|T],X) :- H == 0, verificaFimColuna(T, R), X is R+1.
 
+%%Tenta achar solução
+%jogar(X,Y) :- matrizBase(Matriz), mover(Matriz, X, Y).
+%mover(Matriz, X,Y) :- x == 0, y == 0, moveDireita().
+%mover(Matriz, X,Y) :- x == 0, y == 0, moveAbaixo().
 
-
-%%Descobre quantos elementos tem na lista
-%qntElemLista([],0).
-%qntElemLista([_|T], S) :- qntElemLista(T,G), S is 1+G.
+%%Muda numero em (X,Y)
+%reduz(X,Y) :- recuperaElemento(X, Y, Z).
 
 %%Matriz usada
-matrizBase([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]).
+matrizBase([[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]]).
