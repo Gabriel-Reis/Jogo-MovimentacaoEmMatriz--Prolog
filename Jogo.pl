a6bb667644cd13d754bb4c2e51d9dc7edccaccdb
Gabriel Augusto <g.requenareis@gmail.com>
2019-05-27 19:16:17 -0300

    Caminho único encontrado, escrito em arquivo .txt de saída

--- Jogo.pl
+++ Jogo.pl
@@ -1,15 +1,14 @@
-inicia(X,Y) :- matrizBase(Matriz), imprimeMatriz(Matriz),lengthList(Matriz,T),  jogar(Matriz,X,Y,T).
+inicia(X,Y,Caminho) :- matrizBase(Matriz), imprimeMatriz(Matriz), jogar(Matriz,X,Y,Caminho), escreveArq(Caminho).
 
 %% Imprime a matriz atual do jogo
-imprimeMatriz([]) :- !.
+imprimeMatriz([]).
 imprimeMatriz([H|T]) :- imprimeLista(H), nl, imprimeMatriz(T).
-
 %% Imprime uma lista de elementos
-imprimeLista([]):- !.
-imprimeLista([H|T]) :-
-  write(H), 
-  write(", "),
-  imprimeLista(T).
+imprimeLista([]).
+imprimeLista([H|T]) :- write(H),  write(", "), imprimeLista(T).
+
+escreveArq([]).
+escreveArq(Rota) :- open('caminho.txt', write, ID), writeq(ID, Rota ), write(ID, '.\n'), close(ID).
 
 %% Recupera elemento da lista
 recuperaElemento(X, Y, Elemento,Matriz) :- achaX(X,0,Matriz,ListaX), achaY(Y,0,ListaX,Elemento).
@@ -35,67 +34,27 @@ elementosVizinhos(X,Y,[S,I,D,E],Matriz) :- elementoSuperior(X,Y,S,Matriz), eleme
 
 %%Verifica se jogo chegou ao fim
 fimDeJogo(Matriz) :- verificaFim(Matriz,X), X == 1.
-	verificaFim([],X) :- X is 0, !.
+	verificaFim([],X) :- X is 0.
 	verificaFim([H|T],X) :- verificaFimColuna(H,X1), verificaFim(T,X2), X is X1 + X2.
 	verificaFimColuna([],X) :- X is 0, !.
 	verificaFimColuna([H|T],R) :- H == -1, verificaFimColuna(T, R).
 	verificaFimColuna([H|T],X) :- H == 0, verificaFimColuna(T, R), X is R+1.
 
-%%Tenta achar solução
-%Situacoes de fim de jogo
-jogar(Matriz, _,_,_) :- fimDeJogo(Matriz), !.
-jogar(Matriz, X,Y,_) :- elementosVizinhos(X,Y,[-1,-1,-1,-1],Matriz), false. %4 OPCOES
-jogar(Matriz, 0,0,_) :- elementosVizinhos(0,0,[-5,-1,-1,-5],Matriz), false.	%SUP ESQ.
-jogar(Matriz, 0,T,T) :- elementosVizinhos(0,T,[-5,-1,-5,-1],Matriz), false.	%SUP DIR.
-jogar(Matriz, T,0,T) :- elementosVizinhos(T,0,[-1,-5,-1,-5],Matriz), false.	%INF ESQ.
-jogar(Matriz, T,T,T) :- elementosVizinhos(T,T,[-1,-5,-5,-1],Matriz), false.	%INF DIR.
-%Situacoes do jogador nas cantos
-jogar(Matriz, 0,0,T) :- decrementa(0,0,Matriz,NMAtriz),			 jogar(NMAtriz,  1,  0,T).	%SUP ESQ.
-jogar(Matriz, 0,0,T) :- decrementa(0,0,Matriz,NMAtriz),			 jogar(NMAtriz,  0,  1,T).	%SUP ESQ.
-jogar(Matriz, 0,T,T) :- decrementa(0,T,Matriz,NMAtriz),			 jogar(NMAtriz,  1,  T,T).	%SUP DIR.
-jogar(Matriz, 0,T,T) :- decrementa(0,T,Matriz,NMAtriz),NY is T-1,jogar(NMAtriz,  T, NY,T).	%SUP DIR.
-jogar(Matriz, T,0,T) :- decrementa(T,0,Matriz,NMAtriz),NX is T-1,jogar(NMAtriz, NX,  0,T).	%INF ESQ.
-jogar(Matriz, T,0,T) :- decrementa(T,0,Matriz,NMAtriz),			 jogar(NMAtriz,  T,  1,T).	%INF ESQ.
-jogar(Matriz, T,T,T) :- decrementa(T,T,Matriz,NMAtriz),NX is T-1,jogar(NMAtriz, NX,  T,T).	%INF DIR.
-jogar(Matriz, T,T,T) :- decrementa(T,T,Matriz,NMAtriz),NY is T-1,jogar(NMAtriz,  T, NY,T).	%INF DIR.
-%Situacoes do jogador nas laterais
-jogar(Matriz, X,0,T) :- decrementa(X,0,Matriz,NMAtriz),NX is X+1,jogar(NMAtriz, NX,  0,T).	%LAT ESQ.
-jogar(Matriz, X,0,T) :- decrementa(X,0,Matriz,NMAtriz),NX is X-1,jogar(NMAtriz, NX,  0,T).	%LAT ESQ.
-jogar(Matriz, X,0,T) :- decrementa(X,0,Matriz,NMAtriz),			 jogar(NMAtriz,  X,  1,T).	%LAT ESQ.
-jogar(Matriz, X,T,T) :- decrementa(X,T,Matriz,NMAtriz),NX is X+1,jogar(NMAtriz, NX,  T,T).	%LAT DIR.
-jogar(Matriz, X,T,T) :- decrementa(X,T,Matriz,NMAtriz),NX is X-1,jogar(NMAtriz, NX,  T,T).	%LAT DIR.
-jogar(Matriz, X,T,T) :- decrementa(X,T,Matriz,NMAtriz),NY is T-1,jogar(NMAtriz,  X, NY,T).	%LAT DIR.
-jogar(Matriz, 0,Y,T) :- decrementa(0,Y,Matriz,NMAtriz),			 jogar(NMAtriz,  1,  Y,T).	%LAT SUP.
-jogar(Matriz, 0,Y,T) :- decrementa(0,Y,Matriz,NMAtriz),NY is Y+1,jogar(NMAtriz,  0, NY,T).	%LAT SUP.
-jogar(Matriz, 0,Y,T) :- decrementa(0,Y,Matriz,NMAtriz),NY is Y-1,jogar(NMAtriz,  0, NY,T).	%LAT SUP.
-jogar(Matriz, T,Y,T) :- decrementa(T,Y,Matriz,NMAtriz),NX is T-1,jogar(NMAtriz, NX,  Y,T).	%LAT INF.
-jogar(Matriz, T,Y,T) :- decrementa(T,Y,Matriz,NMAtriz),NY is Y-1,jogar(NMAtriz,  T, NY,T).	%LAT INF.
-jogar(Matriz, T,Y,T) :- decrementa(T,Y,Matriz,NMAtriz),NY is Y-1,jogar(NMAtriz,  T, NY,T).	%LAT INF.
-%Situacoes restantes
-jogar(Matriz, X,Y,T) :- decrementa(X,Y,Matriz,NMAtriz),NX is X-1,jogar(NMAtriz, NX, Y,T).
-jogar(Matriz, X,Y,T) :- decrementa(X,Y,Matriz,NMAtriz),NX is X+1,jogar(NMAtriz, NX, Y,T).
-jogar(Matriz, X,Y,T) :- decrementa(X,Y,Matriz,NMAtriz),NY is Y-1,jogar(NMAtriz, X, NY,T).
-jogar(Matriz, X,Y,T) :- decrementa(X,Y,Matriz,NMAtriz),NY is Y+1,jogar(NMAtriz, X, NY,T).
-
-%procuraX(_, AuxX, MatrizVelha, _) :- tamanhoMatriz(MatrizVelha,Tam), write(Tam), AuxX > Tam.
-%procuraX(_, _, [], []).
-%procuraX(X, AuxX, [HO|TO], [HO|MatrizAtualizada]) :- NovoX is AuxX+1, procuraX(X, NovoX, TO, MatrizAtualizada).
+%%Situacoes restantes
+jogar(Matriz, _,_,Rota) :- fimDeJogo(Matriz), escreveArq(Rota).
+jogar(Matriz, X,Y,["S"|Rota]) :- elementoSuperior(X,Y,Z,Matriz),Z > -1,decrementa(X,Y,Matriz,NMAtriz),NX is X-1,jogar(NMAtriz, NX, Y,Rota).
+jogar(Matriz, X,Y,["I"|Rota]) :- elementoInferior(X,Y,Z,Matriz),Z > -1,decrementa(X,Y,Matriz,NMAtriz),NX is X+1,jogar(NMAtriz, NX, Y,Rota).
+jogar(Matriz, X,Y,["E"|Rota]) :- elementoEsquerda(X,Y,Z,Matriz),Z > -1,decrementa(X,Y,Matriz,NMAtriz),NY is Y-1,jogar(NMAtriz, X, NY,Rota).
+jogar(Matriz, X,Y,["D"|Rota]) :- elementoDireita(X,Y,Z,Matriz) ,Z > -1,decrementa(X,Y,Matriz,NMAtriz),NY is Y+1,jogar(NMAtriz, X, NY,Rota).
 
 %%Muda numero em (X,Y)
 decrementa(X,Y,Matriz, MatrizAtualizada) :- reduzX(X,Y,0,0,Matriz,MatrizAtualizada).
-	
 	reduzX(_,_,    _,    _,    [],         []) .
 	reduzX(X,Y,    X, AuxY, [H|T], [LAlt|Mat]) :- NX is X+1,reduzY(_,Y,   _,  AuxY, H, LAlt),reduzX(X,Y,NX, AuxY, T, Mat).
 	reduzX(X,Y, AuxX, AuxY, [H|T], [   H|Mat]) :- NAuxX is AuxX+1, reduzX(X,Y,NAuxX, AuxY, T,  Mat).
-	
-	reduzY(_,_, _,    _,    [],       []) .
-	reduzY(_,Y, _, AuxY, [H|T], [ H|MAT]) :- AuxY > Y,  NY is AuxY+1, reduzY(_,Y, _, NY, T, MAT).
-	reduzY(_,Y, _,    Y, [H|T], [NH|MAT]) :- NH is H-1, NY is Y+1, 	 reduzY(_,Y, _, NY, T, MAT).
-	reduzY(_,Y, _, AuxY, [H|T], [ H|MAT]) :- 		   NY is AuxY+1, reduzY(_,Y, _, NY, T, MAT).
-
-
-lengthList([],0).
-lengthList([_|R],N):- lengthList(R,N1), N is 1 + N1.
+		reduzY(_,_, _,    _,    [],       []) .
+		reduzY(_,Y, _,    Y, [H|T], [NH|MAT]) :- NH is H-1, NY is Y+1, 	 reduzY(_,Y, _, NY, T, MAT).
+		reduzY(_,Y, _, AuxY, [H|T], [ H|MAT]) :- 		   NY is AuxY+1, reduzY(_,Y, _, NY, T, MAT).
 
 %%Matriz usada
 matrizBase([[1,1],[1,1]]).
