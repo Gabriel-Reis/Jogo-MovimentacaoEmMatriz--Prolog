2d3c16b5ea53e38c73589345ac1828fa916c4221
Gabriel Augusto <g.requenareis@gmail.com>
2019-05-29 19:42:37 -0300

    Jogo funcional, sem bugs conhecidos

--- Jogo.pl
+++ Jogo.pl
@@ -1,5 +1,5 @@
-play(X,Y,Matriz,Caminhos):-bagof(Caminho,inicia(X,Y,Matriz,Caminho),Caminhos).
-inicia(X,Y,Matriz,Caminho) :- jogar(Matriz,X,Y,Caminho), escreveArq(Caminho).
+play(X,Y,Matriz,Caminhos):-bagof(Caminho,inicia(X,Y,Matriz,Caminho),Caminhos),escreveArq(Caminhos).
+inicia(X,Y,Matriz,Caminho) :- jogar(Matriz,X,Y,Caminho).
 
 %% Imprime a matriz atual do jogo
 imprimeMatriz([]).
@@ -9,19 +9,23 @@ imprimeLista([]).
 imprimeLista([H|T]) :- write(H),  write(", "), imprimeLista(T).
 %% Escreve no arquivo
 escreveArq([]).
-escreveArq(Rota) :- open('caminho.txt', write, ID), writeq(ID,Rota), write(ID,'\n'), close(ID).
+escreveArq(Rota) :- open('caminho.txt', write, ID), escreveRota(Rota,ID), close(ID).
+escreveRota([],_) .
+escreveRota([H|T],ID) :- write(ID, H), write(ID,'\n'), escreveRota(T,ID).
 
 %% Recupera elemento da lista
-recuperaElemento(X, Y, Elemento,Matriz) :- achaElemento(X,0,Matriz,ListaX), achaElemento(Y,0,ListaX,Elemento).
+recuperaElemento(X, Y, Elemento,Matriz) :- achaElemento(X,0,Matriz,ListaX), 
+achaElemento(Y,0,ListaX,Elemento).
 %% Acha um elemento.	
 achaElemento(_,_,[],_) :- false.
 achaElemento(Elemento,Aux,[H|_],H) :- Elemento == Aux.
 achaElemento(Elemento,Aux,[_|HT],R) :- Aux1 is Aux + 1, achaElemento(Elemento,Aux1,HT,R).
 
-elementosVizinhos(X,Y,[S,I,D,E],Matriz) :- elementoSuperior(X,Y,S,Matriz), elementoInferior(X,Y,I,Matriz), elementoDireita(X,Y,D,Matriz), elementoEsquerda(X,Y,E,Matriz).
+elementosVizinhos(X,Y,[S,I,D,E],Matriz) :- elementoSuperior(X,Y,S,Matriz), elementoInferior(X,Y,I,Matriz), 
+											elementoDireita(X,Y,D,Matriz), elementoEsquerda(X,Y,E,Matriz).
 %%Descobre vizinhos de (X,Y)
 elementoSuperior(X,Y,Elemento,Matriz) :- N is X-1, recuperaElemento(N,Y,Elemento, Matriz).
-elementoSuperior(_,_,-5,_). 
+elementoSuperior(_,_,-5,_).
 elementoInferior(X,Y,Elemento,Matriz) :- N is X+1, recuperaElemento(N,Y,Elemento,Matriz).
 elementoInferior(_,_,-5,_).
 elementoDireita(X,Y,Elemento,Matriz) :- N is Y+1, recuperaElemento(X,N,Elemento,Matriz).
@@ -43,24 +47,21 @@ impossivelJogar(Matriz, X,Y) :- elementosVizinhos(X,Y,Resp,Matriz), verifica(Res
 
 %%Situacoes restantes
 jogar(Matriz, _,_, []) :- fimDeJogo(Matriz).
-jogar(Matriz, X,Y, _) :- impossivelJogar(Matriz, X,Y).
+jogar(Matriz, X,Y, _) :- impossivelJogar(Matriz, X,Y), fail.
 jogar(Matriz, X,Y,["S"|Rota]) :- elementoSuperior(X,Y,Z,Matriz),Z > -1,decrementa(X,Y,Matriz,NMatriz),
 																						NX is X-1,jogar(NMatriz, NX, Y,Rota).
-jogar(Matriz, X,Y,["I"|Rota]) :- elementoInferior(X,Y,Z,Matriz),Z > -1,decrementa(X,Y,Matriz,NMatriz),
-																						NX is X+1,jogar(NMatriz, NX, Y,Rota).
+jogar(Matriz, X,Y,["I"|Rota]) :- elementoInferior(X,Y,Z,Matriz),Z > -1,
+decrementa(X,Y,Matriz,NMatriz),
+NX is X+1,jogar(NMatriz, NX, Y,Rota).
 jogar(Matriz, X,Y,["E"|Rota]) :- elementoEsquerda(X,Y,Z,Matriz),Z > -1,decrementa(X,Y,Matriz,NMatriz),
 																						NY is Y-1,jogar(NMatriz, X, NY,Rota).
 jogar(Matriz, X,Y,["D"|Rota]) :- elementoDireita(X,Y,Z,Matriz) ,Z > -1,decrementa(X,Y,Matriz,NMatriz),
 																						NY is Y+1,jogar(NMatriz, X, NY,Rota).
-
 %%Muda numero em (X,Y)
 decrementa(X,Y,Matriz, MatrizAtualizada) :- reduzX(X,Y,0,0,Matriz,MatrizAtualizada).
-	reduzX(_,_,    _,    _,    [],         []) .
-	reduzX(X,Y,    X, AuxY, [H|T], [LAlt|Mat]) :- NX is X+1,reduzY(X,Y, AuxY,  AuxY, H, LAlt), reduzX(X,Y,NX, AuxY, T, Mat).
-	reduzX(X,Y, AuxX, AuxY, [H|T], [   H|Mat]) :- NAuxX is AuxX+1, reduzX(X,Y,NAuxX, AuxY, T,  Mat).
-		reduzY(_,_, _,    _,    [],       []) .
-		reduzY(_,Y, _,    Y, [H|T], [NH|MAT]) :- NH is H-1, NY is Y+1, 	 reduzY(_,Y, _, NY, T, MAT).
-		reduzY(_,Y, _, AuxY, [H|T], [ H|MAT]) :- 		   NY is AuxY+1, reduzY(_,Y, _, NY, T, MAT).
 
-%%Matriz usada
-matrizBase([[0,0],[0,0]]).
+reduzX(X,Y,    X, AuxY, [H|T], [LAlt|T]) :- reduzY(X,Y, AuxY,  AuxY, H, LAlt).
+reduzX(X,Y, AuxX, AuxY, [H|T], [   H|Mat]) :- NAuxX is AuxX+1, reduzX(X,Y,NAuxX, AuxY, T,  Mat).
+
+reduzY(_,Y, _,    Y, [H|T], [NH|T]) :- NH is H-1.
+reduzY(_,Y, _, AuxY, [H|T], [ H|MAT]) :- NY is AuxY+1, reduzY(_,Y, _, NY, T, MAT).
